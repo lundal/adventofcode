324708a9775ca535c304ba1cbc44f69284b078a1
Per Thomas Lundal <perthomas@gmail.com>
2017-12-10 14:33:37 +0100

    Add 2017 day 10

---
+++ 2017/src/Day10.elm
@@ -0,0 +1,107 @@
+module Day10 exposing (main)
+
+import Answer
+import Bitwise
+import Char
+import Hex
+import List.Extra
+
+
+rotateLeft : Int -> List a -> List a
+rotateLeft n list =
+    List.drop n list ++ List.take n list
+
+
+rotateRight : Int -> List a -> List a
+rotateRight n list =
+    rotateLeft (List.length list - n) list
+
+
+reverseFirst : Int -> List a -> List a
+reverseFirst n list =
+    List.reverse (List.take n list) ++ List.drop n list
+
+
+hash : Int -> Int -> List Int -> List Int
+hash rounds size lengths =
+    hashInternal rounds 0 0 (List.range 0 (size - 1)) [] lengths
+
+
+hashInternal : Int -> Int -> Int -> List Int -> List Int -> List Int -> List Int
+hashInternal rounds position skipSize input processedLengths remainingLengths =
+    case remainingLengths of
+        head :: tail ->
+            let
+                nextRemainingLengths =
+                    tail
+
+                nextProcessedLengths =
+                    processedLengths ++ [ head ]
+
+                nextInput =
+                    input
+                        |> rotateLeft position
+                        |> reverseFirst head
+                        |> rotateRight position
+
+                nextSkipSize =
+                    skipSize + 1
+
+                nextPosition =
+                    (position + head + skipSize) % List.length input
+            in
+                hashInternal rounds nextPosition nextSkipSize nextInput nextProcessedLengths nextRemainingLengths
+
+        [] ->
+            if rounds <= 1 then
+                input
+            else
+                hashInternal (rounds - 1) position skipSize input [] processedLengths
+
+
+
+-- Part 2
+
+
+parseLengths : String -> List Int
+parseLengths string =
+    (string |> String.toList |> List.map Char.toCode) ++ [ 17, 31, 73, 47, 23 ]
+
+
+densify : List Int -> List Int
+densify sparse =
+    sparse
+        |> List.Extra.groupsOf 16
+        |> List.map (\l -> List.foldl Bitwise.xor 0 l)
+
+
+
+-- Main
+
+
+input1 =
+    [ 199, 0, 255, 136, 174, 254, 227, 16, 51, 85, 1, 2, 22, 17, 7, 192 ]
+
+
+input2 =
+    "199,0,255,136,174,254,227,16,51,85,1,2,22,17,7,192"
+
+
+part1 =
+    input1
+        |> hash 1 256
+        |> List.take 2
+        |> List.product
+
+
+part2 =
+    input2
+        |> parseLengths
+        |> hash 64 256
+        |> densify
+        |> List.map Hex.fromIntMax256
+        |> String.concat
+
+
+main =
+    Answer.render part1 part2
